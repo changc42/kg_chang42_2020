7c6dd76e13f6fd0258019d93a1da30717075b530
Caleb Chang <changc42@gmail.com>
2020-03-17 13:23:45 -0400

    modified approach after clarifying problem. Finsihed problem by checking if s1 has all unique characters, and then checking is s1 has the same length as s2

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+/*
+my understanding of problem:
+one-to-one mapping exists if all of the following are true:
+  1. s1 contains all unique characters
+  2. s1.length = s2.length
+*/
+
 let s1 = process.argv[2];
 let s2 = process.argv[3];
 
-let set = new Set();
+let isAllUnique = true;
+for (let i = 0; i < s1.length; i++) {
+  for (let j = i + 1; j < s1.length; j++) {
+    if (s1.charAt(i) === s1.charAt(j)) {
+      isAllUnique = false;
+      break;
+    }
+  }
+  if (!isAllUnique) break;
+}
 
-let s1Arr = s1.split("");
-s1Arr.forEach(ch => {
-  set.add(ch);
-});
-
-console.log(set);
+if (!isAllUnique) console.log(false);
+else console.log(s1.length === s2.length);
